3a5eed4385555ac005c1affab5ba1111ec2f9a07
Asif <asifrmemon@gmail.com>
2015-08-08 18:43:43 -0700

    Fixing the main.py file since I updated python runtime environment.

--- main.py
+++ main.py
@@ -23,10 +23,17 @@
 #   main ()
 
 import webapp2
+import os
+from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
 
 class MainPage(webapp2.RequestHandler):
-  def get(self):
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.out.write('Hello, WebApp World!')
+  def get(self, q):
+  	if q is None:
+  		q = 'index.html'
 
-app = webapp2.WSGIApplication([('/', MainPage)])
+  	path = os.path.join (os.path.dirname (__file__), q)
+  	self.response.headers['Content-Type'] = 'text/html'
+  	self.response.out.write (template.render (path, {}))
+
+app = webapp2.WSGIApplication([('/(.*html)?', MainPage)])
