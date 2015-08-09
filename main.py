0d91f8fc692fd16a4cca66de62f0808a881aefe5
Asif <asifrmemon@gmail.com>
2015-08-08 17:10:44 -0700

    Updating the app.yaml and main.py file to use a new CGI.

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 # Copyright 2012 Digital Inspiration
 # http://www.labnol.org/
 
-import os
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
-from google.appengine.ext.webapp import template
+# import os
+# from google.appengine.ext import webapp
+# from google.appengine.ext.webapp import util
+# from google.appengine.ext.webapp import template
 
-class MainHandler(webapp.RequestHandler):
-  def get (self, q):
-    if q is None:
-      q = 'index.html'
+# class MainHandler(webapp.RequestHandler):
+#   def get (self, q):
+#     if q is None:
+#       q = 'index.html'
 
-    path = os.path.join (os.path.dirname (__file__), q)
-    self.response.headers ['Content-Type'] = 'text/html'
-    self.response.out.write (template.render (path, {}))
+#     path = os.path.join (os.path.dirname (__file__), q)
+#     self.response.headers ['Content-Type'] = 'text/html'
+#     self.response.out.write (template.render (path, {}))
 
-def main ():
-  application = webapp.WSGIApplication ([('/(.*html)?', MainHandler)], debug=True)
-  util.run_wsgi_app (application)
+# def main ():
+#   application = webapp.WSGIApplication ([('/(.*html)?', MainHandler)], debug=True)
+#   util.run_wsgi_app (application)
 
-if __name__ == '__main__':
-  main ()
+# if __name__ == '__main__':
+#   main ()
+
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/plain'
+    self.response.out.write('Hello, WebApp World!')
+
+app = webapp2.WSGIApplication([('/', MainPage)])
