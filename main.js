124cda54cc7c4eb9127dc1bba670c24082f17565
sinake <asifrmemon@gmail.com>
2015-10-19 08:06:29 -0700

    Changing html and adding in necessary css and js for making pages responsive.

--- main.js
+++ main.js
@@ -15,3 +15,19 @@ $('a[href*=#]').click(function(event){
     event.preventDefault();
     $('html,body').animate({scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top}, 500);
 });
+
+//Clicking a down arrow link will open the related section.
+$('.section-down-link').click(function(){
+	event.preventDefault();
+	$(this).siblings('.section-content').show();
+	$(this).siblings('.section-up-link').children().show();
+	$(this).children().hide();
+});
+
+//Clicking the up arrow link will close the related section
+$('.section-up-link').click(function(){
+	event.preventDefault();
+	$(this).siblings('.section-content').hide();
+	$(this).siblings('.section-down-link').children().show();
+	$(this).children().hide();
+});
