0c306a285ed82a5b9695a18030c508e031c08218
Asif <asifrmemon@gmail.com>
2015-12-16 10:26:07 -0800

    Changing to app.yaml to a php environment, fixing send email php code, and adding in JS to keep user on the main page after sending email.

--- main.php
+++ main.php
@@ -1,22 +1,26 @@
-<?php
+<?php 
+  
+  //Turning on error reporting
+  error_reporting(-1);
+  ini_set('display_errors', 'On');
+  set_error_handler("var_dump");
   
   if(isset($_POST['submit'])) {
 
     //Email information
     $admin_email = "asifrmemon@gmail.com";
     $email = $_POST['email'];
-    $subject = $_POST['subject'];
+    $subject = $_POST['subject']." *From Your Website*";
     $message = $_POST['message'];
-    
-    $headers = "From: $admin_email \r\n";
-    $headers .= "Reply-To: $email \r\n";
-    
+    $headers = "From: ".$admin_email."\r\n".
+    'Reply-To: '.$email;
+
     //send email
     mail($admin_email, $subject, $message, $headers);
     
     //Email response
     echo "Thank you for contacting us!";
-  
-  }
+
+  } 
 
 ?>
