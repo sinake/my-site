e475a0d059d46be296a4316c0742015e2e9234b8
Asif <asifrmemon@gmail.com>
2017-01-21 17:37:15 -0800

    Adding backend verification of recaptcha response.

--- main.php
+++ main.php
@@ -4,20 +4,35 @@
   // error_reporting(-1);
   // ini_set('display_errors', 'On');
   // set_error_handler("var_dump");
-  
+
+  //if form is submitted.
   if(isset($_POST['submit'])) {
 
-    //Email information
-    $admin_email = "asifrmemon@gmail.com";
-    $email = $_POST['email'];
-    $subject = $_POST['subject']." *From Your Website*";
-    $message = $_POST['message'];
-    $headers = "From: ".$admin_email."\r\n".
-    'Reply-To: '.$email;
+    //if captcha response is given
+    if(isset($_POST['g-recaptcha-response']) && $_POST['g-recaptcha-response']) {
 
-    //send email
-    mail($admin_email, $subject, $message, $headers);
+      //test the captcha response for correctness.
+      $secret = "6LfkIBEUAAAAAD1SyJeqyBYu-NhyzPvWr4UpkVtM";
+      $captcha_response = $_POST['g-recaptcha-response'];
+      $verify_url = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$captcha_response");
+      $json_array = json_decode($verify_url,TRUE);
+      
+      //if json_array contains a true response to the success key.
+      if($json_array['success']) {
+        
+        //Email information
+        $admin_email = "asifrmemon@gmail.com";
+        $email = $_POST['email'];
+        $subject = $_POST['subject']." *From Your Website*";
+        $message = $_POST['message'];
+        $headers = "From: ".$admin_email."\r\n".
+        'Reply-To: '.$email;
 
+        //send email
+        mail($admin_email, $subject, $message, $headers);
+
+      }
+    }
   } 
 
 ?>
